c1ae222fb5afba0654986ab09ae45110cfdd7348
Kentaro Ito <kentaroito@kentaronombp.mynet>
2023-04-09 00:08:51 +0800

    名大会話コーパスでファインチューニングしたモデルのコミット

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ if torch.cuda.is_available():
 model = model.to(type)
 
 # 初めの文章
-prompt = "早く日本に帰りたいな。"
+prompt = "私香港に"
 # 生成する文章の数
 num = 1
 
@@ -26,11 +26,11 @@ input_ids = tokenizer.encode(
 with torch.no_grad():
     output = model.generate(
         input_ids,
-        max_length=300,  # 最長の文章長
-        min_length=50,  # 最短の文章長
+        max_length=100,
+        min_length=100,
         do_sample=True,
-        top_k=600,  # 上位{top_k}個の文章を保持
-        top_p=0.90,  # 上位{top_p}%の単語から選択する。例）上位95%の単語から選んでくる
+        top_k=500,
+        top_p=0.95,
         pad_token_id=tokenizer.pad_token_id,
         bos_token_id=tokenizer.bos_token_id,
         eos_token_id=tokenizer.eos_token_id,
